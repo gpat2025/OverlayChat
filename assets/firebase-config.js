0bdf1b64f9f20c61b4ff0dd90e037fd0aa8c57cf
Zoync-tech <zoync1@gmail.com>
2026-03-30 22:49:36 +0530

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyB9bXec79HW7l2Ow812gLTEvLvoiAJRtPY",
-  authDomain: "overlaychat-6f3c1.firebaseapp.com",
-  databaseURL: "https://overlaychat-6f3c1-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "overlaychat-6f3c1",
-  storageBucket: "overlaychat-6f3c1.firebasestorage.app",
-  messagingSenderId: "935528132270",
-  appId: "1:935528132270:web:7dd72409fa1fe9bf10873f",
+  apiKey: "",
+  authDomain: "m",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 export const isFirebaseConfigured = Object.values(firebaseConfig).every(
